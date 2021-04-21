149b3b15c1eadc9af1c0934ffddea07d902be65b
Joel Verhagen <jver@microsoft.com>
2021-04-21 14:01:59 -0700

    Initial apples commit

---
+++ ApplesClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CanHazSourceLink.Apples
+{
+    public class ApplesClass
+    {
+        public void MyMethod()
+        {
+            if (!string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("APPLES")))
+            {
+                Console.WriteLine("apples");
+            }
+        }
+    }
+}
